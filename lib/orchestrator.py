2ef12a25f620cde2690cc5782a858dc3a6799e6f
Marco Baggio <marcobgg4@gmail.com>
2023-08-14 21:00:48 +0200

    Orchestrator now has private datamaster and designer

--- lib/orchestrator.py
+++ lib/orchestrator.py
@@ -1,6 +1,7 @@
 from .DataMaster import DataMaster
 from .Designer import Designer
 from typing import Optional, Literal
+from pandas import DataFrame
 
 
 class Orchestrator:
@@ -8,31 +9,38 @@ class Orchestrator:
     def __init__(self):
         print("Initializing Orchestrator...")
         print("Adding DataMaster...")
-        self.dm = DataMaster()
+        self.__dm = DataMaster()
         print("Adding Designer...")
-        self.ds = Designer()
+        self.__ds = Designer()
 
     def add_stock(
         self,
         ticker: str,
     ) -> None:
-        self.dm.add_ticker(ticker=ticker)
+        self.__dm.add_ticker(ticker=ticker)
 
     def remove_stock(
         self,
         ticker: str,
     ) -> None:
-        self.dm.remove_ticker(ticker=ticker)
+        self.__dm.remove_ticker(ticker=ticker)
 
     def available_stocks(self) -> list:
-        return self.dm.list_tickers()
+        return self.__dm.list_tickers()
+
+    def get_stock_history(
+        self,
+        ticker: str,
+        period: Optional[Literal["1mo", "3mo", "6mo", "1y"]] = "3mo",
+    ) -> DataFrame:
+        return self.__dm.get_ticker_history(ticker=ticker, period=period)
 
     def plot_stock(
         self,
         ticker: str,
         period: Optional[Literal["1mo", "3mo", "6mo", "1y"]] = "3mo",
     ) -> None:
-        data = self.dm.get_ticker_history(ticker=ticker, period=period)
-        name = self.dm.get_ticker_info(ticker=ticker)["shortName"]
-        self.ds.draw_plot(data, name)
-        self.ds.save_plot(data, name)
+        data = self.__dm.get_ticker_history(ticker=ticker, period=period)
+        name = self.__dm.get_ticker_info(ticker=ticker)["shortName"]
+        self.__ds.draw_plot(data, name)
+        self.__ds.save_plot(data, name)
